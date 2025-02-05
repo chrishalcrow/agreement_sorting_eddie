80b70db594bce80b1f26d3500f3482839dd4aa37
chrishalcrow <57948917+chrishalcrow@users.noreply.github.com>
2025-02-05 18:05:24 +0000

    add grouped motion_correction

--- agreement_sorting_eddie/preprocess.py
+++ agreement_sorting_eddie/preprocess.py
@@ -1,3 +1,4 @@
+import numpy as np
 import spikeinterface.full as si
 from Elrond.Helpers import get_chronologized_recording_paths, get_session_names
 
@@ -10,7 +11,7 @@ import sys
 
 mouse = sys.argv[1]
 day = sys.argv[2]
-protocol = sys.argv[3]
+protocol = int(sys.argv[3])
 project_path = sys.argv[4]
 
 deriv_folder = project_path + f"derivatives/M{mouse}/D{day}"
@@ -30,43 +31,50 @@ if mouse <= 21:
 else:
     raw_recordings_list = [si.read_openephys(rec_path) for rec_path in rec_paths]
 
-bad_channels = []
+groups = np.unique(np.array(raw_recordings_list[0].get_property('group')))
 
-for raw in raw_recordings_list:
-    bad_channels_one_rec, _ = si.detect_bad_channels(raw)
-    bad_channels.extend(bad_channels_one_rec)
+bad_channels = []
+for raw_recording in raw_recordings_list:
+    recs_by_group = raw_recording.split_by('group')
+    for rec_group in recs_by_group.values():
+        bad_channels_one_rec, _ = si.detect_bad_channels(rec_group)
+        bad_channels.extend(bad_channels_one_rec)
 
 bad_channels = list(set(bad_channels))
-
-print("Removing bad channels: ", bad_channels)
+print('bad chans: ', bad_channels)
 
 raw_recordings_list = [raw.remove_channels(remove_channel_ids = bad_channels) for raw in raw_recordings_list]
 
-recordings_list = [ si.whiten(si.bandpass_filter(si.phase_shift(raw)),dtype="float32") for raw in raw_recordings_list ]
+recs_per_group={}
+for group in groups:
+        
+    recs_group = [rec.channel_slice(channel_ids=rec.channel_ids[rec.get_property('group')==group]) for rec in raw_recordings_list]
+    pp_recs = [si.whiten(si.bandpass_filter(si.phase_shift(rec_group)),dtype="float32") for rec_group in recs_group]
+        
+    # motion correction per shank
+    recs_and_motions = [si.correct_motion(rec, preset="nonrigid_fast_and_accurate", output_motion=True, output_motion_info=True) for rec in pp_recs]
+    
+    intersected_channels = set(recs_and_motions[0][0].channel_ids)
+    for cm_rec in recs_and_motions[1:]:
+        intersected_channels = intersected_channels.intersection(cm_rec[0].channel_ids)
+    intersected_channels = list(intersected_channels)
+    
+    all_cm_recs_intersect = [cm_rec[0].channel_slice(channel_ids=list(intersected_channels)) for cm_rec in recs_and_motions]
+      
+    peaks_list = [ rec_and_motion[2]['peaks'] for rec_and_motion in recs_and_motions]
+    peak_locations_list = [ rec_and_motion[2]['peak_locations'] for rec_and_motion in recs_and_motions]
+    
+    estimate_histogram_kwargs = get_estimate_histogram_kwargs()
+    estimate_histogram_kwargs["histogram_type"] = "activity_2d"  # TODO: RENAME
 
-recs_and_motions = [si.correct_motion(rec, preset="nonrigid_fast_and_accurate", output_motion=True, output_motion_info=True) for a, rec in enumerate(recordings_list)]
+    corrected_recordings_list, extra_info = align_sessions(
+        all_cm_recs_intersect,
+        peaks_list,
+        peak_locations_list,
+        estimate_histogram_kwargs=estimate_histogram_kwargs
+    )
 
-intersected_channels = set(recs_and_motions[0][0].channel_ids)
-for cm_rec in recs_and_motions[1:]:
-    intersected_channels = intersected_channels.intersection(cm_rec[0].channel_ids)
-intersected_channels = list(intersected_channels)
+    recs_per_group[group] = si.concatenate_recordings(corrected_recordings_list)
 
-all_cm_recs_intersect = [cm_rec[0].channel_slice(channel_ids=list(intersected_channels)) for cm_rec in recs_and_motions]
-
-peaks_list = [ rec_and_motion[2]['peaks'] for rec_and_motion in recs_and_motions]
-peak_locations_list = [ rec_and_motion[2]['peak_locations'] for rec_and_motion in recs_and_motions]
-
-estimate_histogram_kwargs = get_estimate_histogram_kwargs()
-estimate_histogram_kwargs["histogram_type"] = "activity_2d"  # TODO: RENAME
-
-corrected_recordings_list, extra_info = align_sessions(
-    all_cm_recs_intersect,
-    peaks_list,
-    peak_locations_list,
-    estimate_histogram_kwargs=estimate_histogram_kwargs
-)
-
-print("shifts: ", extra_info)
-
-rec = si.concatenate_recordings(corrected_recordings_list)
-rec.save_to_folder(f"{deriv_folder}/full/rec_preprocessing_whitened_corrected_{protocol}")
+full_rec = si.aggregate_channels(list(recs_per_group.values()))
+full_rec.save_to_folder(f"{deriv_folder}/full/rec_preprocessing_whitened_corrected_{protocol}")
