20801400e0171026973f1bbc2b6eb712862b89a6
chrishalcrow <57948917+chrishalcrow@users.noreply.github.com>
2025-02-18 12:20:47 +0000

    import return protocols

--- agreement_sorting_eddie/preprocess.py
+++ agreement_sorting_eddie/preprocess.py
@@ -2,6 +2,7 @@ import numpy as np
 import spikeinterface.full as si
 from Elrond.Helpers import get_chronologized_recording_paths, get_session_names
 import sys
+from defaults import return_protocols
 
 from spikeinterface.preprocessing.inter_session_alignment.session_alignment import (
     get_estimate_histogram_kwargs,
